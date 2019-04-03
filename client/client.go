a5f1aa350d89da2746700fae64c0c025b45f59b0
Kevin <862860000@qq.com>
2019-04-03 19:53:15 +0800

    add func

--- client/client.go
+++ client/client.go
@@ -9,8 +9,14 @@ type Clienter interface {
 	WatchConfig()
 	Get(key string) interface{}
 	GetBool(key string) bool
+	GetFloat32(key string) float32
 	GetFloat64(key string) float64
 	GetInt(key string) int
+	GetInt32(key string) int32
+	GetInt64(key string) int64
+	GetUint(key string) uint
+	GetUint32(key string) uint32
+	GetUint64(key string) uint64
 	GetString(key string) string
 	GetTime(key, timeLayout string) (time.Time, error)
 	GetDuration(key string) time.Duration
